8e350302d615b9202b0f61b343da204e23e1d72c
Utkersh Rajvenshi <utkarshrajvanshi2@gmail.com>
2024-03-22 21:38:25 +0530

    Added ChakraUI and ReactRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    )
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
