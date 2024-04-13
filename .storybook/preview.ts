b31d3f0310766dfbae31710d70ef0b74cf5ea7e2
utkershrajvenshi <utkarshrajvanshi2@gmail.com>
2024-04-13 13:31:31 +0000

    Tailwind and story configuration

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,3 +1,4 @@
+import '../src/index.css'; // replace with the name of your tailwind css file
 import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
   ],
   prefix: "",
   theme: {
+    fontFamily: {
+      serif: "'Hepta Slab', Arial, serif",
+    },
     container: {
       center: true,
       padding: "2rem",
