a587797c5a77b0f93b538962a95f22243449a2b6
utkershrajvenshi <utkarshrajvanshi2@gmail.com>
2024-04-17 15:07:20 +0000

    Added custom components

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,6 +25,7 @@ module.exports = {
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
+        'success-green': '#2C8B2A',
         foreground: "hsl(var(--foreground))",
         primary: {
           DEFAULT: "hsl(var(--primary))",
